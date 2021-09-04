21258fd81e4fe5affcda253ab30186c89a070726
Shubham <shubhamkakade061@gmail.com>
2021-09-04 17:06:55 +0530

    First Commit

---
+++ app/src/main/java/com/skappdevelopement/e_bloodbank/MainActivity.java
@@ -0,0 +1,41 @@
+package com.skappdevelopement.e_bloodbank;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView  nav_view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("E-BloodBank");
+
+        nav_view = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this,drawerLayout,
+                toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+}
---
+++ app/src/main/java/com/skappdevelopement/e_bloodbank/RegistrationActivity.java
@@ -0,0 +1,51 @@
+package com.skappdevelopement.e_bloodbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+
+    private Button donorButton;
+    private Button recipientButton;
+    private TextView backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        donorButton = findViewById(R.id.donorButton);
+        recipientButton = findViewById(R.id.recipientButton);
+        backButton = findViewById(R.id.backButton);
+
+        recipientButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this,RecipientRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        donorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this,DonorRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistrationActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
